d0257a001a8e651a0b00efdf337546a3d4ee0fc9
cfitzsimonds <cfitzsimonds@gmail.com>
2016-01-19 11:08:26 -0500

    First commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# Angular Check SP Permissions — Demo Application for Angular JS
+
+This project is a sample application demonstrating a more fully developed realistic single page angular application.
+
+### Server
+
+A node server is provided. From the command line run "npm install" and then run "server.bat" from the command line.
+
+### Running the application
+
+Navigate your browser to `http://localhost:8000/` to see the app running in your browser. The port is configured in the node server.
+
---
+++ app/js/controllers/EventController.js
@@ -0,0 +1,41 @@
+'use strict';
+
+eventsApp.controller('EventController',
+	function EventController($scope) {
+
+		$scope.event = {
+			name: 'Angular Boot Camp',
+			date: '1/15/2016',
+			time: '03:02 pm',
+			location: {
+				address: 'Google Headquarters',
+				city: 'Mountain View',
+				province: 'CA'
+			},
+			imageUrl: '/img/angularjs-logo.png',
+			sessions: [
+				{
+					name: 'Directive Masterclass',
+					creatorName: 'Bob Smith',
+					duration: '1 hr',
+					level: 'Advanced',
+					abstract: 'In this session you will learn the ins and outs of directives!'
+				},
+				{
+					name: 'Scopes for fun and profit',
+					creatorName: 'John Doe',
+					duration: '30 mins',
+					level: 'Introductory',
+					abstract: 'This session will take a closer look at scopes. Learn what they do, how they do it, and how to get them to do it for you.'
+				},
+				{
+					name: 'Well Behaved Controllers',
+					creatorName: 'Jane Doe',
+					duration: '2 hours',
+					level: 'Intermediate',
+					abstract: 'Controllers are the beginning of everything Angular does. Learn how to craft controllers that will win the respect of your friends and neighbors.'
+				}
+			]
+		}
+	}
+);
